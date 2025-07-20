4d158978bb32e92fc8e713a9a68a58f7925dde30
PantheraNestah <pantheranestah@gmail.com>
2025-07-20 23:04:24 +0300

    updating serializers.py to add fields appropriately

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -4,9 +4,11 @@ from .models import Listing, Booking
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = '__all__'
+        # Name each field you want to include in the API
+        fields = ['id', 'title', 'description', 'price', 'owner']
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
+        # Name each field for the booking endpoint
+        fields = ['id', 'listing', 'guest', 'check_in_date', 'check_out_date']
